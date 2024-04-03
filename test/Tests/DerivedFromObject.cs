1ef289c1cf1964c46e52ba8b399a2fbecdecdfeb
Daniel Weber <danielcweber@users.noreply.github.com>
2024-04-03 14:50:49 +0200

    Add a failing test that does not event execute the VerifySettings-constructor before.

--- test/Tests/DerivedFromObject.cs
+++ test/Tests/DerivedFromObject.cs
@@ -1,4 +1,5 @@
-﻿using VerifyTests;
+﻿using System;
+using VerifyTests;
 using Xunit;
 
 namespace Tests
@@ -15,6 +16,15 @@ namespace Tests
         public void Fails_always()
         {
             new VerifySettings();
+            new org.apache.commons.configuration2.BaseConfiguration();
+        }
+
+        [Fact]
+        public void Fails_always_even_VerifySettings_is_not_even_constructed()
+        {
+            if (DateTime.Now.Month == 13)
+                new VerifySettings();
+
             new org.apache.commons.configuration2.BaseConfiguration();
         }
     }
